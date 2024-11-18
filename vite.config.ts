e416dc3b19ce6f208535a2ba11dbf26482412c0a
ATHman3 <40130169+ATHman3@users.noreply.github.com>
2024-11-18 11:25:43 +0000

    enhance Vite config to include build options for asset directory and output naming

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/passport_dz_3d/",
-})
+  base: "/passport_dz_3d/", // Exact repository name
+  publicDir: 'public',
+  assetsInclude: ['**/*.glb'],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'passport.glb') {
+            return '[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
+});
