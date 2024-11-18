f53d2c7cc275af97e777a491a94f0052af568519
ATHman3 <ouzeriat.athmane@gmail.com>
2024-11-18 19:32:28 +0100

    add LICENSE, update index.html title, refactor App component layout, and enhance Scene component lighting and controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import Scene from './components/Scene'
 import './App.css'
 
+
+
 function App() {
+  // TODO: add passport text
+  // TODO: resposive grid layout
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Scene />
+    <div className="h-screen flex items-center justify-center">
+      <div  className="bg-white p-6  w-[1200px] shadow-2xl grid lg:grid-cols-2 gap-4 justify-center">
+
+        <div id='passport' className='w-[500px] h-[500px] bg-white shadow-2xl '>
+          <Scene />
+        </div>
+
+        <p className="text-black">Lorem, ipsum dolor sit amet consectetur adipisicing elit. A veritatis, in eaque ut id consequuntur. Maxime incidunt ipsa laudantium sapiente est, praesentium provident nesciunt voluptatem a quasi amet dolore velit!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat possimus nobis cum quis reiciendis accusamus sit ipsum quisquam voluptas, deserunt placeat perspiciatis nulla aspernatur sed aliquam. Itaque debitis amet animi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptas, dolorem accusamus maiores, corrupti, aliquam recusandae magnam voluptatibus adipisci earum molestias hic repudiandae facilis possimus dolore aperiam. Minima, deleniti! Soluta?
+        </p>
+      </div>
     </div>
   )
 }
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -5,14 +5,19 @@ import { FC } from 'react'
 import Model from './Model'
 
 const Scene: FC = () => {
-  return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[0, 0, 1]} />
-      <Model scale={2} />
-      <OrbitControls />
-    </Canvas>
-  )
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[0, 0, 1]} />
+            <Model scale={2.4} />
+            <OrbitControls
+                enableZoom={false}
+                maxPolarAngle={Math.PI / 2}
+                minPolarAngle={Math.PI / 2}
+                target={[0, 0, 0]}
+            />
+        </Canvas>
+    )
 }
 
 export default Scene
