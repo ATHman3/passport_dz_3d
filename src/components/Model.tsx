71774056fbdb07c386c0afbb10b368670386168f
ATHman3 <40130169+ATHman3@users.noreply.github.com>
2024-11-18 11:44:29 +0000

    update Model component to use dynamic base URL for GLB file and preload model

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -3,10 +3,13 @@ import { GroupProps } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
 
 const Model: FC<GroupProps> = (props) => {
-  // Remplacez le chemin par l'emplacement de votre fichier GLB
-  const { scene } = useGLTF('/passport.glb')
+  const modelPath = `${import.meta.env.BASE_URL}passport.glb`
+  const { scene } = useGLTF(modelPath)
   
   return <primitive object={scene} {...props} />
 }
+
+// Preload the model
+useGLTF.preload(`${import.meta.env.BASE_URL}passport.glb`)
 
 export default Model
