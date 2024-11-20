1e360591a2861ffbe837dcffd408b35032fac5f8
ATHman3 <40130169+ATHman3@users.noreply.github.com>
2024-11-20 13:13:54 +0000

    refactor App component and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import Scene from './components/Scene'
 import './App.css'
+import CardScene from './components/CardScene'
+import Description from './components/Description'
 
 
 
 function App() {
-  // TODO: add passport text
-  // TODO: resposive grid layout
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <div  className="bg-white p-6  w-[1200px] shadow-2xl grid lg:grid-cols-2 gap-4 justify-center">
-
-        <div id='passport' className='w-[500px] h-[500px] bg-white shadow-2xl '>
-          <Scene />
-        </div>
-
-        <p className="text-black">Lorem, ipsum dolor sit amet consectetur adipisicing elit. A veritatis, in eaque ut id consequuntur. Maxime incidunt ipsa laudantium sapiente est, praesentium provident nesciunt voluptatem a quasi amet dolore velit!
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat possimus nobis cum quis reiciendis accusamus sit ipsum quisquam voluptas, deserunt placeat perspiciatis nulla aspernatur sed aliquam. Itaque debitis amet animi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptas, dolorem accusamus maiores, corrupti, aliquam recusandae magnam voluptatibus adipisci earum molestias hic repudiandae facilis possimus dolore aperiam. Minima, deleniti! Soluta?
-        </p>
+      <div className="bg-white p-6  w-[1200px] shadow-2xl grid lg:grid-cols-2 gap-4 justify-center">
+        <CardScene />
+        <Description />
       </div>
     </div>
   )
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,21 +1,16 @@
 // src/components/Scene.tsx
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls,OrbitControlsProps } from '@react-three/drei'
 import { FC } from 'react'
-import Model from './Model'
+import Passport from './Passport'
 
-const Scene: FC = () => {
+const Scene: FC<OrbitControlsProps> = (props) => {
     return (
         <Canvas>
             <ambientLight />
             <pointLight position={[0, 0, 1]} />
-            <Model scale={2.4} />
-            <OrbitControls
-                enableZoom={false}
-                maxPolarAngle={Math.PI / 2}
-                minPolarAngle={Math.PI / 2}
-                target={[0, 0, 0]}
-            />
+            <Passport scale={2.4} />
+            <OrbitControls {...props}/>
         </Canvas>
     )
 }
