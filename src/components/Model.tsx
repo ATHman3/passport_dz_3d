2b7f618710f3456a9008befb2f8003681b33e80a
Anonyme <athman3@proton.me>
2024-11-20 13:08:17 +0100

    update Model component to use relative path for GLB file and preload model

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -3,13 +3,13 @@ import { GroupProps } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
 
 const Model: FC<GroupProps> = (props) => {
-  const modelPath = `${import.meta.env.BASE_URL}passport.glb`
+  const modelPath = `passport.glb`
   const { scene } = useGLTF(modelPath)
   
   return <primitive object={scene} {...props} />
 }
 
 // Preload the model
-useGLTF.preload(`${import.meta.env.BASE_URL}passport.glb`)
+useGLTF.preload(`passport.glb`)
 
 export default Model
